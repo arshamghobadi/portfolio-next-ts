20be71d0c99fb11c6083d8c05660e92a443997f5
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-02-03 15:54:59 +0000

    end of projects UI component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,10 @@ import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
 
+import WorkExperience from '@/components/WorkExperience';
+import Skills from '@/components/Skills';
+import Projects from '@/components/Projects';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
@@ -22,6 +26,15 @@ export default function Home() {
       </section>
       <section id="About" className="snap-center">
         <About />
+      </section>
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
+      <section id="projects" className="snap-start">
+        <Projects />
       </section>
     </div>
   );
